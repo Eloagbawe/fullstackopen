882692c194e5172c3c5b23913a714cb609175be1
Eloagbawe <eloagbawe@gmail.com>
2020-10-09 23:29:51 +0100

    phonebook step1 solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,27 +1,53 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './components/person.js'
+
 
 
 const App = () => {
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
   ]) 
-  const [ newName, setNewName ] = useState('')
+  const [ newName, setNewName ] = useState('...a new name')
+
+  const handleNameChange = (event) => {
+        setNewName(event.target.value)
+  }
+  
+  const addName = (event) => {
+    event.preventDefault()
+    
+    const nameObject = {
+      name: newName
+    }  
+
+    for (var x in persons){
+      if (persons[x].name === newName){
+        alert(`${newName} is already added to phonebook`)
+      }
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" >add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <p>Arto Hellas</p>
+      <ul>
+        {persons.map(person => 
+          <Person key={person.name} person={person} />
+        )}
+      </ul>
     </div>
   )
 }
