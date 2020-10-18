b8c823f5ed224e3eeda7eb898ecfff92fd962e95
Eloagbawe <eloagbawe@gmail.com>
2020-10-18 23:23:05 +0100

    countries step 1 solution

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,10 +2,37 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 import './App.css';
 
-function App() {
-  const [countries, setCountries] = useState([])
+
+const Search = ({filteredCountries}) =>{
+  return(
+    <div>{filteredCountries.map(country => {
+      if (filteredCountries.length > 10){
+        return <p key={country.name}>Too many matches, specify another filter</p>
+      }
+      if (filteredCountries.length === 1){
+       return <div key = {country.name}>
+         <h1>{country.name}</h1>
+         <p>Capital {country.capital}</p>
+         <p>Population {country.population}</p>
+         <h2>Languages</h2>
+         <ul>{country.languages.map(language => <li key = {language.name}>{language.name}</li>)}</ul>
+         <img src = {country.flag} alt = "" width = '400px' height ='200px'></img>
+       </div>
+       
+     } 
+     else {
+        return <p key = {country.name}>{country.name}</p>
+     }
+     })
+     }
+    </div>
+  )
+}
+
+const App = () => {
+  const [ countries, setCountries ] = useState([])
   const [ filter, setFilter ] = useState('')
-  const [filteredCountries, setFilteredCountries] = useState([])
+  const [ filteredCountries, setFilteredCountries ] = useState([])
  
 
   useEffect(() => {
@@ -16,15 +43,21 @@ function App() {
       })
   }, [])
 
-  const handleSearch = (event) =>{
+  const handleSearch = (event) => {
     setFilter(event.target.value)
+    if (event.target.value !== ''){
     const regex = new RegExp(`^${event.target.value}`,"i")
     setFilteredCountries(countries.filter(country => regex.test(country.name)))
+    }
+    else { 
+      setFilteredCountries([])
+    }
   }
+  
   return (
     <div>
       find countries <input value = {filter} onChange={handleSearch}/>
-     <div>{filteredCountries.map(country => <li key = {country.name}>{country.name}</li>)}</div>
+     <Search filteredCountries = {filteredCountries}/>
     </div>
   );
 }
