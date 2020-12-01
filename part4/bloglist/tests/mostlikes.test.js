c2d5de2b99bfdc7201e48ba78a3d53150943a0d0
Eloagbawe <eloagbawe@gmail.com>
2020-12-01 23:18:13 +0100

    minor fix

--- part4/bloglist/tests/mostlikes.test.js
+++ part4/bloglist/tests/mostlikes.test.js
@@ -52,10 +52,68 @@ describe('most likes', () => {
              __v: 0 
             }
     ]
+
+    const listWithManyBlogs2 = [ 
+        { 
+            _id: "5a422a851b54a676234d17f7", 
+            title: "React patterns", 
+            author: "Robert C. Martin", 
+            url: "https://reactpatterns.com/", 
+            likes: 7, 
+            __v: 0 
+        }, 
+        { 
+            _id: "5a422aa71b54a676234d17f8", 
+            title: "Go To Statement Considered Harmful", 
+            author: "Edsger W. Dijkstra", 
+            url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html", 
+            likes: 5, 
+            __v: 0 
+        }, 
+        { 
+            _id: "5a422b3a1b54a676234d17f9", 
+            title: "Canonical string reduction", 
+            author: "Edsger W. Dijkstra", 
+            url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html", 
+            likes: 12, 
+            __v: 0 
+        }, 
+        { 
+            _id: "5a422ba71b54a676234d17fb", 
+            title: "TDD harms architecture", 
+            author: "Michael Chan", 
+            url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html", 
+            likes: 0, 
+            __v: 0 
+        }, 
+        { 
+            _id: "5a422bc61b54a676234d17fc", 
+            title: "Type wars", 
+            author: "Michael Chan", 
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", 
+            likes: 2,
+             __v: 0 
+        },
+        { 
+            _id: "5a422bc61b54a676234d17fd", 
+            title: "Type wars", 
+            author: "Robert C. Martin", 
+            url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", 
+            likes: 10,
+             __v: 0 
+            }
+        ]
     test('when list has many blogs, it is calculated right', () => {
         const result = listHelper.mostLikes(listWithManyBlogs)
         expect(result).toEqual({ 
             author: "Edsger W. Dijkstra", 
+            likes: 17
+        })
+    })
+    test('when list has more than one top blogger, it returns any one of them', () => {
+        const result = listHelper.mostLikes(listWithManyBlogs2)
+        expect(result).toEqual({ 
+            author: "Robert C. Martin", 
             likes: 17
         })
     })
