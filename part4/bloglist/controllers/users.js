c80c4e81f548f6f0d0fb707b86605dd8af0da3c1
Eloagbawe <eloagbawe@gmail.com>
2020-12-17 22:45:33 +0100

    user api

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -1,10 +1,20 @@
 const bcrypt = require('bcrypt')
-const { request } = require('../app')
 const usersRouter = require('express').Router
 const User = require('../models/user')
 
 usersRouter.post('/', async(req,res) => {
     const body = request.body
     const saltrounds = 10
-    const hashedPassword = await bcrypt.hash(body.password, saltrounds )
+    const passwordHash = await bcrypt.hash(body.password, saltrounds)
+
+    const user = new User({
+        username: body.username,
+        name: body.name,
+        passwordHash
+    })
+
+    const savedUser = await user.save()
+    res.json(savedUser)
 })
+
+module.exports = usersRouter
