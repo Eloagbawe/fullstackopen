48735f20aedeeb9d995fa84aa3d088aaf0ecf619
Eloagbawe <eloagbawe@gmail.com>
2022-03-23 23:32:06 +0100

    update app.js

--- part5/bloglist/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist/bloglist-frontend/src/services/blogs.js
@@ -5,5 +5,7 @@ const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
-
-export default { getAll }
+const blogService = {
+  getAll
+}
+export default blogService
