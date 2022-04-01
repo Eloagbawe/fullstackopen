59fbc6d77763336e45f6fe643ea25ca0650d43b8
Eloagbawe <eloagbawe@gmail.com>
2022-04-01 22:29:55 +0100

    add user log in/out

--- part5/bloglist/bloglist-frontend/src/App.js
+++ part5/bloglist/bloglist-frontend/src/App.js
@@ -19,6 +19,17 @@ const App = () => {
     )  
   }, [])
 
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+
   const handleLogin = async (event) => {
     event.preventDefault()
     console.log('logging in with', username, password)
@@ -27,6 +38,10 @@ const App = () => {
       const user = await loginService.login({
         username, password,
       })
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
@@ -38,6 +53,12 @@ const App = () => {
     }
 
     
+  }
+
+  const handleLogout = (event) =>{
+    event.preventDefault()
+    window.localStorage.removeItem('loggedBlogappUser')
+    setUser(null)
   }
 
 
@@ -73,10 +94,13 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-     <p> {user.username} is logged in</p>
+     <p> {user.username} is logged in</p>     
+     <button type="submit" onClick={handleLogout}>logout</button>
+
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
+
     </div>
   )
 
