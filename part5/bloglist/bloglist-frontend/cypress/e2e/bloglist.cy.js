9db9245b38a4b057ad2c480cacd5ae9a44ee4f5b
Eloagbawe <eloagbawe@gmail.com>
2023-05-28 22:26:46 +0100

    add login tests to bloglist.cy.js

--- part5/bloglist/bloglist-frontend/cypress/e2e/bloglist.cy.js
+++ part5/bloglist/bloglist-frontend/cypress/e2e/bloglist.cy.js
@@ -1,11 +1,21 @@
 describe('Blog list app', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', function() {
     cy.contains('Blogs')
     cy.contains('log in')
   })
-  // it('front page contains random text', function() {
-  //   cy.visit('http://localhost:3000')
-  //   cy.contains('wtf is this app?')
-  // })
+  it('login form can be opened', function() {
+    cy.contains('log in').click()
+  })
+  it('user can login', function () {
+    cy.contains('log in').click()
+    cy.get('#username').type('root')
+    cy.get('#password').type('sekret')
+    cy.get('#login-button').click()
+
+    cy.contains('root user is logged in')
+  })
 })
