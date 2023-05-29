6e60e0df91a13f55d9984b5248789d12d3b67b6d
Eloagbawe <eloagbawe@gmail.com>
2023-05-29 23:43:27 +0100

    add tests for creating a new blog

--- part5/bloglist/bloglist-frontend/cypress/e2e/bloglist.cy.js
+++ part5/bloglist/bloglist-frontend/cypress/e2e/bloglist.cy.js
@@ -1,5 +1,12 @@
 describe('Blog list app', function() {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Root User',
+      username: 'root',
+      password: 'sekret'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -16,6 +23,24 @@ describe('Blog list app', function() {
     cy.get('#password').type('sekret')
     cy.get('#login-button').click()
 
-    cy.contains('root user is logged in')
+    cy.contains('Root User is logged in')
+  })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('root')
+      cy.get('#password').type('sekret')
+      cy.get('#login-button').click()
+    })
+
+    it('a new blog can be created', function() {
+      cy.contains('create new blog').click()
+      cy.get('#title').type('a new blog')
+      cy.get('#author').type('cypress')
+      cy.get('#url').type('localhost')
+      cy.get('#create').click()
+      cy.contains('a new blog cypress')
+    })
   })
 })
--- part5/bloglist/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist/bloglist-frontend/src/components/BlogForm.js
@@ -26,6 +26,7 @@ const BlogForm = ({ createBlog  }) => {
             type="text"
             value={title}
             name="title"
+            id="title"
             onChange={({ target }) => setTitle(target.value)}
           />
         </div>
@@ -35,6 +36,7 @@ const BlogForm = ({ createBlog  }) => {
             type="text"
             value={author}
             name="author"
+            id="author"
             onChange={({ target }) => setAuthor(target.value)}
           />
         </div>
@@ -45,11 +47,12 @@ const BlogForm = ({ createBlog  }) => {
             type="text"
             value={url}
             name="url"
+            id="url"
             onChange={({ target }) => setUrl(target.value)}
           />
         </div>
 
-        <button type="submit">create</button>
+        <button type="submit" id="create">create</button>
       </form>
     </div>
   )
