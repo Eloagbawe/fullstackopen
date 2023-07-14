6de71e5478ab4f87d6b5c0637c56989645b91e78
Eloagbawe <eloagbawe@gmail.com>
2023-07-14 23:39:09 +0100

    update notification reducer

--- part7/extended-bloglist/bloglist-frontend/src/reducers/notificationReducer.js
+++ part7/extended-bloglist/bloglist-frontend/src/reducers/notificationReducer.js
@@ -1,19 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+export const setNotification = (message, propertyName, time) => {
+  return (dispatch, getState) => {
+    clearTimeout(getState().notification.timeOutId)
+    const timeOutId = setTimeout(() => {
+      dispatch(removeNotification())
+    }, time * 1000)
+    dispatch(addNotification({ message, timeOutId, propertyName }))
+  }
+}
+
 const notificationSlice = createSlice({
   name: 'notification',
   initialState: {
-    message: ''
+    message: null,
+    timeOutId: undefined,
+    propertyName: undefined
   },
   reducers: {
     addNotification(state, action){
+      const { message, timeOutId, propertyName } = action.payload
       return {
-        message: action.payload.message
+        message,
+        timeOutId,
+        propertyName
+      }
+    },
+    removeNotification(state){
+      return {
+        ...state,
+        message: null,
+        propertyName: undefined
       }
     }
   }
 })
 
-export const { addNotification } = notificationSlice.actions
+export const { addNotification, removeNotification } = notificationSlice.actions
 
 export default notificationSlice.reducer
