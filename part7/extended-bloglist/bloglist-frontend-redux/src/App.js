9872ca6ea8a0e616247286f2edcf7800cab406f2
Eloagbawe <eloagbawe@gmail.com>
2023-07-20 14:57:23 +0100

    add index, users, blog detail and user detail routes

--- part7/extended-bloglist/bloglist-frontend-redux/src/App.js
+++ part7/extended-bloglist/bloglist-frontend-redux/src/App.js
@@ -1,40 +1,27 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.css'
-import Blog from './components/Blog'
 import Notification from './components/notification'
 import LoginForm from './components/LoginForm'
-import BlogForm from './components/BlogForm'
 import Togglable from './components/Togglable'
 import { setNotification } from './reducers/notificationReducer'
 import { useDispatch, useSelector } from 'react-redux'
-import { getAllBlogs, addBlog, updateBlogLikes, removeBlog } from './reducers/blogReducer'
 import { initializeUser, logoutUser, loginUser } from './reducers/authReducer'
-import { createSelector } from '@reduxjs/toolkit'
+import { Routes, Route } from 'react-router-dom'
+import Users from './pages/users'
+import Blogs from './pages/blogs'
+import User from './pages/user'
+import BlogDetail from './pages/blog'
 
 const App = () => {
   const dispatch = useDispatch()
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const blogFormRef = useRef()
 
   const { message, propertyName } = useSelector(state => state.notification)
 
-  const blogs = useSelector(createSelector(
-    state => state.blog.blogs,
-    (blogs) => {
-      const blogList = [...blogs]
-      return blogList.sort(function (a, b) {
-        return b.likes - a.likes
-      })
-    }
-
-  ))
   const user = useSelector(state => state.auth)
 
-  useEffect(() => {
-    dispatch(getAllBlogs())
-  }, [])
 
   useEffect(() => {
     dispatch(initializeUser())
@@ -57,29 +44,6 @@ const App = () => {
     dispatch(logoutUser())
   }
 
-  const createBlog = async(blogObject) => {
-    blogFormRef.current.toggleVisibility()
-    try {
-      const loggedInUser = {
-        id: user.id,
-        name: user.name,
-        username: user.username,
-      }
-      const returnedBlog = await dispatch(addBlog(blogObject, loggedInUser))
-      dispatch(setNotification(
-        `a new blog ${returnedBlog.title} by ${returnedBlog.author} added`,
-        'success', 5))
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  const blogForm = () => (
-    <Togglable buttonLabel="create new blog" ref={blogFormRef}>
-      <BlogForm createBlog={createBlog} />
-    </Togglable>
-  )
-
   const loginForm = () => (
     <Togglable buttonLabel="log in">
       <LoginForm
@@ -92,51 +56,28 @@ const App = () => {
     </Togglable>
   )
 
-  const addLike = async(blog) => {
-    const updatedBlog = { ...blog, likes: blog.likes + 1, user: blog.user.id }
-    try {
-      await dispatch(updateBlogLikes(blog.id, updatedBlog))
-    } catch (err) {
-      console.log(err)
-    }
+  if (user === null) {
+    return (
+      loginForm()
+    )
   }
 
-  const deleteBlog = async(blog) => {
-    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
-      try {
-        await dispatch(removeBlog(blog.id))
-      } catch (err) {
-        console.log(err)
-      }
-    }
-  }
   return (
     <div>
-      <h1>Blogs</h1>
       <Notification message={message} propertyName={propertyName} />
-      {user === null ? (
-        loginForm()
-      ) : (
-        <div>
-          <h2>blogs</h2>
-          <p> {user.name} is logged in</p>
-          <button type="submit" onClick={handleLogout}>
-            logout
-          </button>
-          {blogForm()}
-          <br />
-          <div>
-            { blogs && blogs.map((blog) => (
-              <Blog
-                key={blog.id}
-                blog={blog}
-                addLike={addLike}
-                deleteBlog={deleteBlog}
-              />
-            ))}
-          </div>
-        </div>
-      )}
+      <div>
+        <h2>blogs</h2>
+        <p> {user.name} is logged in</p>
+        <button type="submit" onClick={handleLogout}>
+          logout
+        </button>
+      </div>
+      <Routes>
+        <Route path="/" element={<Blogs/>} />
+        <Route path="/users" element={<Users/>} />
+        <Route path="/users/:id" element={<User/>} />
+        <Route path="/blogs/:id" element={<BlogDetail/>} />
+      </Routes>
     </div>
   )
 }
