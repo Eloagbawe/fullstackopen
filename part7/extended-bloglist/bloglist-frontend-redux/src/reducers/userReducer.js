791b6a6bade1aa6d3d1ebbd4c01c8553dc994ad9
Eloagbawe <eloagbawe@gmail.com>
2023-07-20 14:21:00 +0100

    add user service and user reducer

---
+++ part7/extended-bloglist/bloglist-frontend-redux/src/reducers/userReducer.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+import userService from '../services/users'
+
+const userSlice = createSlice({
+  name: 'users',
+  initialState: null,
+  reducers: {
+    getUsers(state, action) {
+      return action.payload
+    }
+  }
+})
+
+export const getAllUsers = () => {
+  return async dispatch => {
+    const usersData = await userService.getUsers()
+    dispatch(getUsers(usersData))
+    return usersData
+  }
+}
+export const { getUsers } = userSlice.actions
+
+export default userSlice.reducer
