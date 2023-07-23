ce8292efb09f69327b14fa720048ecfb78e3651b
Eloagbawe <eloagbawe@gmail.com>
2023-07-23 23:13:46 +0100

    add react router dom and page routes

--- part7/extended-bloglist/bloglist-frontend-query/src/App.js
+++ part7/extended-bloglist/bloglist-frontend-query/src/App.js
@@ -1,26 +1,25 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import './index.css'
-import Blog from './components/Blog'
 import Notification from './components/notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import LoginForm from './components/LoginForm'
-import BlogForm from './components/BlogForm'
 import Togglable from './components/Togglable'
 import { useContext } from 'react'
-import { useQuery, useMutation, useQueryClient } from 'react-query'
+import Blogs from './pages/blogs'
+import Users from './pages/users'
+import User from './pages/user'
+import BlogDetail from './pages/blog'
+import { Routes, Route } from 'react-router-dom'
 
 import notificationContext from './store/notificationContext'
 import authContext from './store/authContext'
 
 const App = () => {
-  const queryClient = useQueryClient()
-
   const [notification, notificationDispatch] = useContext(notificationContext)
   const [user, userDispatch] = useContext(authContext)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const blogFormRef = useRef()
 
   const { message, propertyName } = notification
 
@@ -32,49 +31,9 @@ const App = () => {
         type: 'SET USER',
         payload: user
       })
-      blogService.setToken(user)
+      blogService.setToken(user.token)
     }
   }, [])
-  const result = useQuery('blogs', blogService.getAll, {
-    retry: 1,
-    refetchOnWindowFocus: false,
-  })
-
-  const blogs = result.data
-    ? result.data.sort(function (a, b) {
-      return b.likes - a.likes
-    })
-    : []
-
-  const addBlogMutation = useMutation(blogService.create, {
-    onSuccess: (data) => {
-      queryClient.invalidateQueries('blogs')
-      notificationDispatch({
-        type: 'DISPLAY MESSAGE',
-        payload: {
-          message: `a new blog ${data.title} by ${data.author} added`,
-          propertyName: 'success',
-        },
-      })
-      setTimeout(() => {
-        notificationDispatch({
-          type: 'CLEAR MESSAGE',
-        })
-      }, 5000)
-    },
-  })
-
-  const addLikeMutation = useMutation(blogService.update, {
-    onSuccess: () => {
-      queryClient.invalidateQueries('blogs')
-    },
-  })
-
-  const deleteBlogMutation = useMutation(blogService.deleteBlog, {
-    onSuccess: () => {
-      queryClient.invalidateQueries('blogs')
-    },
-  })
 
   const handleLogin = async (event) => {
     event.preventDefault()
@@ -90,7 +49,7 @@ const App = () => {
         type: 'SET USER',
         payload: user
       })
-      blogService.setToken(user)
+      blogService.setToken(user.token)
       setUsername('')
       setPassword('')
     } catch (exception) {
@@ -117,17 +76,6 @@ const App = () => {
     })
   }
 
-  const createBlog = (blogObject) => {
-    blogFormRef.current.toggleVisibility()
-    addBlogMutation.mutate(blogObject)
-  }
-
-  const blogForm = () => (
-    <Togglable buttonLabel="create new blog" ref={blogFormRef}>
-      <BlogForm createBlog={createBlog} />
-    </Togglable>
-  )
-
   const loginForm = () => (
     <Togglable buttonLabel="log in">
       <LoginForm
@@ -140,52 +88,32 @@ const App = () => {
     </Togglable>
   )
 
-  const addLike = (blog) => {
-    const updatedBlog = { ...blog, likes: blog.likes + 1, user: blog.user.id }
-    addLikeMutation.mutate({ id: blog.id, newObject: updatedBlog })
-  }
-
-  const deleteBlog = (blog) => {
-    if (window.confirm(`Remove blog ${blog.title} by ${blog.author}`)) {
-      deleteBlogMutation.mutate(blog.id)
-    }
-  }
-
-  if (result.isLoading) {
-    return <div>loading data...</div>
-  }
-
-  if (result.isError && result.error.response.status === 500) {
-    return <div>blog service not available due to problems in server</div>
+  if (user === null) {
+    return (
+      <div>
+        <Notification message={message} propertyName={propertyName} />
+        {loginForm()}
+      </div>
+    )
   }
   return (
     <div>
       <h1>Blogs</h1>
       <Notification message={message} propertyName={propertyName} />
-      {user === null ? (
-        loginForm()
-      ) : (
-        <div>
-          <h2>blogs</h2>
-          <p> {user.name} is logged in</p>
-          <button type="submit" onClick={handleLogout}>
-            logout
-          </button>
-          {blogForm()}
-          <br />
-          <div>
-            {blogs &&
-              blogs.map((blog) => (
-                <Blog
-                  key={blog.id}
-                  blog={blog}
-                  addLike={addLike}
-                  deleteBlog={deleteBlog}
-                />
-              ))}
-          </div>
-        </div>
-      )}
+      <div>
+        <h2>blogs</h2>
+        <p> {user.name} is logged in</p>
+        <button type="submit" onClick={handleLogout}>
+          logout
+        </button>
+      </div>
+      <Routes>
+        <Route path="/" element={<Blogs />} />
+        <Route path="/blogs/:id" element={<BlogDetail />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/users/:id" element={<User />} />
+      </Routes>
+
     </div>
   )
 }
--- part7/extended-bloglist/bloglist-frontend-query/src/index.js
+++ part7/extended-bloglist/bloglist-frontend-query/src/index.js
@@ -3,17 +3,20 @@ import ReactDOM from 'react-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { NotificationContextProvider } from './store/notificationContext'
 import { AuthContextProvider } from './store/authContext'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 const queryClient = new QueryClient()
 
 ReactDOM.render(
   <QueryClientProvider client={queryClient}>
-    <AuthContextProvider>
-      <NotificationContextProvider>
-        <App />
-      </NotificationContextProvider>
-    </AuthContextProvider>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <NotificationContextProvider>
+          <App />
+        </NotificationContextProvider>
+      </AuthContextProvider>
+    </BrowserRouter>
   </QueryClientProvider>,
   document.getElementById('root')
 )
