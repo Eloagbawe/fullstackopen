4d779daff5630d05dc86e9f539f536b9c5560c03
Eloagbawe <eloagbawe@gmail.com>
2023-07-27 21:55:11 +0100

    implement app navigation using react router

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,24 +1,23 @@
-import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import { Routes, Route, Link } from 'react-router-dom'
 
 const App = () => {
-  const [page, setPage] = useState('authors')
-
   return (
     <div>
       <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        <button><Link to="/">authors</Link></button>
+        <button><Link to="/books">books</Link></button>
+        <button><Link to="/add_book">add book</Link></button>
       </div>
 
-      <Authors show={page === 'authors'} />
-
-      <Books show={page === 'books'} />
-
-      <NewBook show={page === 'add'} />
+      <Routes>
+        <Route path="/" element={<Authors/>}/>
+        <Route path="/authors" element={<Authors/>}/>
+        <Route path="/books" element={<Books/>}/>
+        <Route path="/add_book" element={<NewBook/>}/>
+      </Routes>
     </div>
   )
 }
