a71350b720aa0443a45c74607d69af3568809648
Eloagbawe <eloagbawe@gmail.com>
2023-07-28 16:55:45 +0100

    implement the authors view and update author birth year functionality

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { ALL_AUTHORS, EDIT_AUTHOR } from "../queries";
+
 const Authors = () => {
-  const authors = []
+  const result = useQuery(ALL_AUTHORS);
+  const [name, setName] = useState("");
+  const [birthYear, setBirthYear] = useState("");
+
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    editAuthor({ variables: { name, birthYear: parseInt(birthYear) } });
+    setName("");
+    setBirthYear("");
+  };
+  const authors = result.data.allAuthors;
 
   return (
     <div>
@@ -20,8 +42,33 @@ const Authors = () => {
           ))}
         </tbody>
       </table>
-    </div>
-  )
-}
 
-export default Authors
+      <h3>Set birthyear</h3>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="name">Name: </label>
+          <select id="name" onChange={(e) => setName(e.target.value)}>
+            {authors.map((a, key) => (
+              <option key={key} value={a.name}>
+                {a.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="birthYear">Born: </label>
+          <input
+            name="birthYear"
+            id="birthYear"
+            type="number"
+            value={birthYear}
+            onChange={(e) => setBirthYear(e.target.value)}
+          />
+        </div>
+        <button type="submit">Update Author</button>
+      </form>
+    </div>
+  );
+};
+
+export default Authors;
