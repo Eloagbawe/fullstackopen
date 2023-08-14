4b1023f249ac9ab67bd089213ea113cfc365ef55
Eloagbawe <eloagbawe@gmail.com>
2023-08-15 01:16:13 +0300

    add POST /exercises endpoint

--- part9/exercises_9.1-9.7/index.ts
+++ part9/exercises_9.1-9.7/index.ts
@@ -1,24 +1,43 @@
-import express from 'express';
-import { calculateBmi } from './bmiCalculator';
-const app = express();
+import express from "express";
+import { calculateBmi } from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
 
-app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
+const app = express();
+app.use(express.json());
+
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!");
 });
 
-app.get('/bmi', (req, res) => {
-  const { height, weight } = req.query
+app.get("/bmi", (req, res) => {
+  const { height, weight } = req.query;
   try {
     const result = calculateBmi(Number(height), Number(weight));
     res.json({
       height,
       weight,
-      bmi: result
-    })
+      bmi: result,
+    });
   } catch (err) {
     res.json({
-      error: 'malformatted parameters'
-    })
+      error: "malformatted parameters",
+    });
+  }
+});
+
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { target, daily_exercises } = req.body;
+  if (!target || !daily_exercises) {
+    res.status(400).json({error: "parameters missing"});
+  }
+  try {
+    const result = calculateExercises(daily_exercises as Array<number>, Number(target));
+    res.json(result);
+  } catch (err) {
+    res.json({
+      error: "malformatted parameters",
+    });
   }
 });
 
