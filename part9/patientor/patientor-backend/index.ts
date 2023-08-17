d14e013352c719c541bce06c4d3f0042e011b21a
Eloagbawe <eloagbawe@gmail.com>
2023-08-17 23:50:51 +0300

    fix cors error

--- part9/patientor/patientor-backend/index.ts
+++ part9/patientor/patientor-backend/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
+app.use((cors as (options: cors.CorsOptions) => express.RequestHandler)({}));
 
-const PORT = 3000;
 
-app.get('/ping', (_req, res) => {
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
 
