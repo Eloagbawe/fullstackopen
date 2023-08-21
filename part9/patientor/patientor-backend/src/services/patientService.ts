37b1778d89d817eca91cf3f40e7b409c3ee2b390
Eloagbawe <eloagbawe@gmail.com>
2023-08-21 23:50:15 +0100

    update patient service

--- part9/patientor/patientor-backend/src/services/patientService.ts
+++ part9/patientor/patientor-backend/src/services/patientService.ts
@@ -1,5 +1,6 @@
 import { Patient } from "../types";
 import data from "../../data/patients";
+import { v4 as uuidv4 } from 'uuid';
 
 const getPatients = (): Omit<Patient, 'ssn'>[] => {
    const response =  data.map(({id, name, dateOfBirth, gender, occupation}) => 
@@ -13,6 +14,21 @@ const getPatients = (): Omit<Patient, 'ssn'>[] => {
     return response;
 };
 
+const addPatient = (name: string, dateOfBirth: string, ssn: string, gender: string, occupation: string): Patient => {
+  const newPatient = {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+    id: uuidv4().toString(),
+    name,
+    dateOfBirth,
+    gender,
+    ssn,
+    occupation,
+  };
+  data.push(newPatient);
+  return newPatient;
+};
+
 export default {
-    getPatients
+    getPatients,
+    addPatient
 };
