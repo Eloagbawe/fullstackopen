b905bdfef09a5dd6aa1fcb27633b4965d465ef70
Eloagbawe <eloagbawe@gmail.com>
2023-08-29 23:08:14 +0100

    render flight diaries on screen

--- part9/flight-diary/flight-diary-frontend/src/App.tsx
+++ part9/flight-diary/flight-diary-frontend/src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import { DiaryEntry } from './types';
 
 import './App.css';
 
 function App() {
+  const [ diaries, setDiaries ] = useState<DiaryEntry[]>([]);
 
-
+  useEffect(() => {
+    axios.get('http://localhost:3001/api/diaries').then((data) => {
+      setDiaries(data.data as DiaryEntry[])
+    })
+  }, [])
+  
   return (
     <div className="App">
-     <h2>Entries</h2>
+     <h2>Diary Entries</h2>
+     {diaries.map((diary, index) => (
+      <div key={index} style={{padding: "0.5rem 0"}}>
+        <h3>{diary?.date}</h3>
+        <p>visibility: {diary?.visibility}</p>
+        <p>weather: {diary?.weather}</p>
+      </div>
+     ))}
     </div>
   );
 }
